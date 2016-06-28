112debf8eb5b1728eab8b7c9e5a3afbc097e589b
Colm Tuite <colmtuite@gmail.com>
2016-06-28 02:37:21 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Default theme for Bantam CSS framework. Created by <a href="http://www.twitter.com/colmtuite" target="_blank">Colm Tuite</a>.
---
+++ scripts/postcss.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const postcss = require('postcss');
+const plugins = [
+    require('autoprefixer'),
+    require('precss'),
+    require("postcss-custom-properties")
+];
+const argv = require('yargs')
+    .demand(2)
+    .alias('m', 'minify')
+    .boolean('minify')
+    .argv;
+
+if (argv.m) {
+    plugins.push(require('cssnano'));
+}
+
+// paths
+const root = path.join(__dirname, '..');
+const input = path.join(root, argv._[0]);
+const output = path.join(root, argv._[1]);
+
+const css = fs.readFileSync(input, 'utf8')
+postcss(plugins)
+    .process(css, { from: input, to: output })
+    .then(function (result) {
+        fs.writeFileSync(output, result.css);
+        console.info('\x1b[32m', `CSS built to ${output}`);
+    }, function(err) {
+        console.error('\x1b[31m', err.message);
+    });
